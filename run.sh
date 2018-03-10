1bb40c9b740072da4155161e13d6faae47b3df7d
Jamendub <jamendub@mgmail.com>
2018-03-10 16:24:31 +0100

    copy .tpl mods config file to regular place

--- run.sh
+++ run.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-if [ -f "/dst/mods/dedicated_server_mods_setup.lua" ]
-then
-    cp -f /dst/mods/dedicated_server_mods_setup.lua /dedicated_server_mods_setup.lua.bak
-    echo 'mods config file dedicated_server_mods_setup.lua moved'
-fi
-
 /steam/steamcmd.sh +login anonymous +force_install_dir /dst +app_update 343050 validate +quit
 
-if [ -f "/dedicated_server_mods_setup.lua.bak" ]
+if [ -f "/dst/mods/dedicated_server_mods_setup.lua.tpl" ]
 then
-    cp -f /dedicated_server_mods_setup.lua.bak /dst/mods/dedicated_server_mods_setup.lua
-    echo 'mods config file dedicated_server_mods_setup.lua restored'
+    cp -f /dst/mods/dedicated_server_mods_setup.lua.tpl /dst/mods/dedicated_server_mods_setup.lua
+    echo 'mods config file dedicated_server_mods_setup.lua.tpl copied to dedicated_server_mods_setup.lua'
 fi
 
 cd /dst/bin
