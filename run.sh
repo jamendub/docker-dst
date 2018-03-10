ff4fe8ebf25e18ccbed5c5ea7f5bb0d3c79a5143
Jamendub <jamendub@mgmail.com>
2018-03-10 18:36:19 +0100

    adding modverrides.lua.tpl file for world configurations

--- run.sh
+++ run.sh
@@ -8,6 +8,13 @@ then
     echo 'mods config file dedicated_server_mods_setup.lua.tpl copied to dedicated_server_mods_setup.lua'
 fi
 
+if [ -f "/dst/mods/modoverrides.lua.tpl" ]
+then
+    cp -f /dst/mods/modoverrides.lua.tpl /dst/cluster/Master/modoverrides.lua
+    cp -f /dst/mods/modoverrides.lua.tpl /dst/cluster/Caves/modoverrides.lua
+    echo 'mods config file modoverrides.lua.tpl applied to master and cave server'
+fi
+
 cd /dst/bin
 command=(./dontstarve_dedicated_server_nullrenderer -persistent_storage_root /dst -conf_dir / -cluster cluster)
 "${command[@]}" -shard Caves  | sed 's/^/Caves:  /' &
